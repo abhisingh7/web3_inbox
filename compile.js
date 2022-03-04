268de399e7a231968098d267e62fe66532d25b62
Abhishek Bhadauriya <abhishekbhadauriya7@gmail.com>
2022-03-04 16:46:10 +0530

    Creating test and using mocha and ganache

--- compile.js
+++ compile.js
@@ -1,9 +1,14 @@
-const path = require('path');
+// Run command to install solidity compiler
+// npm install solc@<your solidity compilerversion>
+const path = require('path'); // It will give path to the root project directory i.e inbox
 const fs = require('fs');
 const solc = require('solc');
 
+// This will generate path to Inbox.sol
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+// reading contents of the file
 const source = fs.readFileSync(inboxPath, 'utf-8');
 
 //console.log(solc.compile(source,1));
+// Making this compiled file available to other files in our project so we're exporting.
 module.exports = solc.compile(source,1).contracts[':Inbox'];
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,58 @@
-// contract test code will go here
+// Run command => npm install mocha ganache-cli web3
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');   // constructor
+const web3 = new Web3(ganache.provider()); // instance to connect with local test network (ganache)
+
+//Mocha is a test running framework. we can use mocha to test any kind of js code we want. 
+//It is a general purpose testing framework
+
+// code for testing mocha
+// command to run test => npm run test
+
+// class Car {
+//     park() {
+//         return 'stopped';
+//     }
+
+//     drive() {
+//         return 'vroom';
+//     }
+// }
+
+// let car;
+// beforeEach(() => {
+//     car = new Car();
+// });
+
+// describe('Car', () => {
+//     it('can park', () => {     
+//         assert.equal(car.park(), 'stopped');
+//     });
+
+//     it('can drive', () => {
+//         assert.equal(car.drive(), 'vroom');
+//     });
+// });
+
+let accounts;
+
+beforeEach(async () => {
+    // Get a list of all accounts
+    // Note := every function we call tied to web3 returns a promise
+    // creating a promise using .then
+    // web3.eth.getAccounts().then(fetchedAccounts => {
+    //         console.log(fetchedAccounts);
+    //     });
+
+    // Note: when we use await then mark async in the function that contains await
+    accounts = await web3.eth.getAccounts();
+
+    // Use one of those accounts to deploy the contract
+});
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        console.log(accounts);
+    });
+});
