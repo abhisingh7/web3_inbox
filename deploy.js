b4266c3a31abb02069301004435a55005d1199f4
Abhishek Bhadauriya <abhishekbhadauriya7@gmail.com>
2022-03-05 17:41:32 +0530

    project completed

--- deploy.js
+++ deploy.js
@@ -1,1 +1,29 @@
-// deploy code will go here
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+// putting metamask 12 words nemonic and infura.io node address in provider.
+const provider = new HDWalletProvider(
+    'stay school hello attitude run vocal canvas adjust cloth tonight relax battle',
+    'https://rinkeby.infura.io/v3/871786a0ec3d4f94b81fa099d02113ef'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .send({ gas: '1000000', from: accounts[0] });
+    
+    console.log('Contract deployed to', result.options.address);
+};
+
+deploy();
+
+// node deploy.js
+// After successful deployment , copy deployed contract address and go to
+// https://rinkeby.etherscan.io/ 
+// and paste and go and check the contract on our test network
